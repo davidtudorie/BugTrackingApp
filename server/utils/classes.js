1391f708365ac7745641768023148a047857a9b5
David <tudoriedavid22@stud.ase.ro>
2025-01-05 20:44:23 +0200

    First commit

---
+++ server/utils/classes.js
@@ -0,0 +1,6 @@
+const users = [
+    {id:1, name:'John', age:25, role: 'TST'},
+    {id:2, name:'Jane', age:22, role: 'MP'},
+    {id:3, name:'Doe', age:30, role: 'TST'},
+    {id:4, name:'Smith', age:35, role: 'TST'},
+]
